71c40fc31a6ccf977fae3b3296b1759009fa2bc9
Matt Richardson <mattr23893@gmail.com>
2024-04-14 18:27:58 +0100

    Started defining variables and a class

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
 # Script file for Terminal Game Project
+
+class Question:
+    question_counter = 0
+
+    def __init__(self, question="Question", correct_answer=""):
+        self.question = question
+        self.correct_answer = ""
+        Question.question_counter += 1
+        self.question_id = Question.question_counter
+    
+answered_correctly = False
+
